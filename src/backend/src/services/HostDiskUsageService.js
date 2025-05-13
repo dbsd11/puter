663a84bbf46aa77efe70b3aef63e8dc3116f94e3
dbsd11 <bitdbsd11@163.com>
2025-05-13 14:09:49 +0800

    Update HostDiskUsageService.js

--- src/backend/src/services/HostDiskUsageService.js
+++ src/backend/src/services/HostDiskUsageService.js
@@ -55,12 +55,13 @@ class HostDiskUsageService extends BaseService {
 
         // Setting the available space to a large number for unhandled platforms 
         var free_space = 1e+14;
-
+        
+        var mountpointConfig = process.env.mountpoint;
         if (current_platform == "darwin") {
-            const mountpoint = this.get_darwin_mountpoint(process.cwd());
+            const mountpoint = mountpointConfig || this.get_darwin_mountpoint(process.cwd());
             free_space = this.get_disk_capacity_darwin(mountpoint);
         } else if (current_platform == "linux") {
-            const mountpoint = this.get_linux_mountpint(process.cwd());
+            const mountpoint = mountpointConfig || this.get_linux_mountpint(process.cwd());
             free_space = this.get_disk_capacity_linux(mountpoint);
         } else if (current_platform == "win32") {
             this.log.warn('HostDiskUsageService: Windows is not supported yet');
